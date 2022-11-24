5e735cbcc10f0de1aa10ffd196a82b2fc6d2da3a
Aleksandar <a.garkov@student.fontys.nl>
2022-11-24 13:38:28 +0100

    Added Login/Register Form

--- Housing_Project/Housing_Project/LoginRegister.Designer.cs
+++ Housing_Project/Housing_Project/LoginRegister.Designer.cs
@@ -28,19 +28,236 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.loginbtn = new System.Windows.Forms.Button();
+            this.registerbtn = new System.Windows.Forms.Button();
+            this.fullnametxt = new System.Windows.Forms.TextBox();
+            this.phonenumbertxt = new System.Windows.Forms.TextBox();
+            this.emailadresstxt = new System.Windows.Forms.TextBox();
+            this.fullnamelbl = new System.Windows.Forms.Label();
+            this.phonenumberlbl = new System.Windows.Forms.Label();
+            this.emailadresslbl = new System.Windows.Forms.Label();
+            this.passwordtxt = new System.Windows.Forms.TextBox();
+            this.passwordlbl = new System.Windows.Forms.Label();
+            this.loginpasswordtxt = new System.Windows.Forms.TextBox();
+            this.loginemailtxt = new System.Windows.Forms.TextBox();
+            this.loginemaillbl = new System.Windows.Forms.Label();
+            this.loginpasswordlbl = new System.Windows.Forms.Label();
+            this.wrongcredentialslbl = new System.Windows.Forms.Label();
+            this.registerbtnshow = new System.Windows.Forms.Button();
+            this.loginshowbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // loginbtn
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.loginbtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.loginbtn.Location = new System.Drawing.Point(80, 487);
+            this.loginbtn.Name = "loginbtn";
+            this.loginbtn.Size = new System.Drawing.Size(205, 64);
+            this.loginbtn.TabIndex = 0;
+            this.loginbtn.Text = "Log In";
+            this.loginbtn.UseVisualStyleBackColor = true;
+            this.loginbtn.Click += new System.EventHandler(this.loginbtn_Click);
+            // 
+            // registerbtn
+            // 
+            this.registerbtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.registerbtn.Location = new System.Drawing.Point(411, 487);
+            this.registerbtn.Name = "registerbtn";
+            this.registerbtn.Size = new System.Drawing.Size(212, 64);
+            this.registerbtn.TabIndex = 1;
+            this.registerbtn.Text = "Register";
+            this.registerbtn.UseVisualStyleBackColor = true;
+            this.registerbtn.Click += new System.EventHandler(this.registerbtn_Click);
+            // 
+            // fullnametxt
+            // 
+            this.fullnametxt.Location = new System.Drawing.Point(80, 49);
+            this.fullnametxt.MinimumSize = new System.Drawing.Size(150, 46);
+            this.fullnametxt.Name = "fullnametxt";
+            this.fullnametxt.Size = new System.Drawing.Size(543, 46);
+            this.fullnametxt.TabIndex = 2;
+            // 
+            // phonenumbertxt
+            // 
+            this.phonenumbertxt.Location = new System.Drawing.Point(80, 138);
+            this.phonenumbertxt.MinimumSize = new System.Drawing.Size(150, 46);
+            this.phonenumbertxt.Name = "phonenumbertxt";
+            this.phonenumbertxt.Size = new System.Drawing.Size(543, 46);
+            this.phonenumbertxt.TabIndex = 3;
+            // 
+            // emailadresstxt
+            // 
+            this.emailadresstxt.Location = new System.Drawing.Point(80, 238);
+            this.emailadresstxt.MinimumSize = new System.Drawing.Size(150, 46);
+            this.emailadresstxt.Name = "emailadresstxt";
+            this.emailadresstxt.Size = new System.Drawing.Size(543, 46);
+            this.emailadresstxt.TabIndex = 4;
+            // 
+            // fullnamelbl
+            // 
+            this.fullnamelbl.AutoSize = true;
+            this.fullnamelbl.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.fullnamelbl.Location = new System.Drawing.Point(276, 9);
+            this.fullnamelbl.Name = "fullnamelbl";
+            this.fullnamelbl.Size = new System.Drawing.Size(138, 37);
+            this.fullnamelbl.TabIndex = 5;
+            this.fullnamelbl.Text = "Full name:";
+            // 
+            // phonenumberlbl
+            // 
+            this.phonenumberlbl.AutoSize = true;
+            this.phonenumberlbl.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.phonenumberlbl.Location = new System.Drawing.Point(262, 98);
+            this.phonenumberlbl.Name = "phonenumberlbl";
+            this.phonenumberlbl.Size = new System.Drawing.Size(197, 37);
+            this.phonenumberlbl.TabIndex = 6;
+            this.phonenumberlbl.Text = "Phone number:";
+            // 
+            // emailadresslbl
+            // 
+            this.emailadresslbl.AutoSize = true;
+            this.emailadresslbl.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.emailadresslbl.Location = new System.Drawing.Point(262, 187);
+            this.emailadresslbl.Name = "emailadresslbl";
+            this.emailadresslbl.Size = new System.Drawing.Size(173, 37);
+            this.emailadresslbl.TabIndex = 7;
+            this.emailadresslbl.Text = "Email Adress:";
+            // 
+            // passwordtxt
+            // 
+            this.passwordtxt.Location = new System.Drawing.Point(80, 327);
+            this.passwordtxt.MinimumSize = new System.Drawing.Size(150, 46);
+            this.passwordtxt.Name = "passwordtxt";
+            this.passwordtxt.Size = new System.Drawing.Size(543, 46);
+            this.passwordtxt.TabIndex = 8;
+            // 
+            // passwordlbl
+            // 
+            this.passwordlbl.AutoSize = true;
+            this.passwordlbl.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.passwordlbl.Location = new System.Drawing.Point(276, 287);
+            this.passwordlbl.Name = "passwordlbl";
+            this.passwordlbl.Size = new System.Drawing.Size(134, 37);
+            this.passwordlbl.TabIndex = 9;
+            this.passwordlbl.Text = "Password:";
+            // 
+            // loginpasswordtxt
+            // 
+            this.loginpasswordtxt.Location = new System.Drawing.Point(80, 287);
+            this.loginpasswordtxt.MinimumSize = new System.Drawing.Size(150, 46);
+            this.loginpasswordtxt.Name = "loginpasswordtxt";
+            this.loginpasswordtxt.Size = new System.Drawing.Size(543, 46);
+            this.loginpasswordtxt.TabIndex = 10;
+            // 
+            // loginemailtxt
+            // 
+            this.loginemailtxt.Location = new System.Drawing.Point(80, 178);
+            this.loginemailtxt.MinimumSize = new System.Drawing.Size(150, 46);
+            this.loginemailtxt.Name = "loginemailtxt";
+            this.loginemailtxt.Size = new System.Drawing.Size(543, 46);
+            this.loginemailtxt.TabIndex = 11;
+            // 
+            // loginemaillbl
+            // 
+            this.loginemaillbl.AutoSize = true;
+            this.loginemaillbl.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.loginemaillbl.Location = new System.Drawing.Point(262, 135);
+            this.loginemaillbl.Name = "loginemaillbl";
+            this.loginemaillbl.Size = new System.Drawing.Size(173, 37);
+            this.loginemaillbl.TabIndex = 12;
+            this.loginemaillbl.Text = "Email Adress:";
+            // 
+            // loginpasswordlbl
+            // 
+            this.loginpasswordlbl.AutoSize = true;
+            this.loginpasswordlbl.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.loginpasswordlbl.Location = new System.Drawing.Point(280, 238);
+            this.loginpasswordlbl.Name = "loginpasswordlbl";
+            this.loginpasswordlbl.Size = new System.Drawing.Size(134, 37);
+            this.loginpasswordlbl.TabIndex = 13;
+            this.loginpasswordlbl.Text = "Password:";
+            // 
+            // wrongcredentialslbl
+            // 
+            this.wrongcredentialslbl.AutoSize = true;
+            this.wrongcredentialslbl.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.wrongcredentialslbl.ForeColor = System.Drawing.Color.Red;
+            this.wrongcredentialslbl.Location = new System.Drawing.Point(229, 409);
+            this.wrongcredentialslbl.Name = "wrongcredentialslbl";
+            this.wrongcredentialslbl.Size = new System.Drawing.Size(241, 37);
+            this.wrongcredentialslbl.TabIndex = 14;
+            this.wrongcredentialslbl.Text = "Wrong credentials!";
+            // 
+            // registerbtnshow
+            // 
+            this.registerbtnshow.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.registerbtnshow.Location = new System.Drawing.Point(411, 487);
+            this.registerbtnshow.Name = "registerbtnshow";
+            this.registerbtnshow.Size = new System.Drawing.Size(212, 64);
+            this.registerbtnshow.TabIndex = 15;
+            this.registerbtnshow.Text = "Register";
+            this.registerbtnshow.UseVisualStyleBackColor = true;
+            this.registerbtnshow.Click += new System.EventHandler(this.registerbtn_Click_1);
+            // 
+            // loginshowbtn
+            // 
+            this.loginshowbtn.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.loginshowbtn.Location = new System.Drawing.Point(80, 487);
+            this.loginshowbtn.Name = "loginshowbtn";
+            this.loginshowbtn.Size = new System.Drawing.Size(205, 64);
+            this.loginshowbtn.TabIndex = 16;
+            this.loginshowbtn.Text = "Log In";
+            this.loginshowbtn.UseVisualStyleBackColor = true;
+            this.loginshowbtn.Click += new System.EventHandler(this.loginshowbtn_Click);
+            // 
+            // LoginRegister
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(706, 614);
+            this.Controls.Add(this.loginshowbtn);
+            this.Controls.Add(this.registerbtnshow);
+            this.Controls.Add(this.wrongcredentialslbl);
+            this.Controls.Add(this.loginpasswordlbl);
+            this.Controls.Add(this.loginemaillbl);
+            this.Controls.Add(this.loginemailtxt);
+            this.Controls.Add(this.loginpasswordtxt);
+            this.Controls.Add(this.passwordlbl);
+            this.Controls.Add(this.passwordtxt);
+            this.Controls.Add(this.emailadresslbl);
+            this.Controls.Add(this.phonenumberlbl);
+            this.Controls.Add(this.fullnamelbl);
+            this.Controls.Add(this.emailadresstxt);
+            this.Controls.Add(this.phonenumbertxt);
+            this.Controls.Add(this.fullnametxt);
+            this.Controls.Add(this.registerbtn);
+            this.Controls.Add(this.loginbtn);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "LoginRegister";
             this.Text = "Login/Register";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button loginbtn;
+        private Button registerbtn;
+        private TextBox fullnametxt;
+        private TextBox phonenumbertxt;
+        private TextBox emailadresstxt;
+        private Label fullnamelbl;
+        private Label phonenumberlbl;
+        private Label emailadresslbl;
+        private TextBox passwordtxt;
+        private Label passwordlbl;
+        private TextBox loginpasswordtxt;
+        private TextBox loginemailtxt;
+        private Label loginemaillbl;
+        private Label loginpasswordlbl;
+        private Label wrongcredentialslbl;
+        private Button registerbtnshow;
+        private Button loginshowbtn;
     }
 }
--- Housing_Project/Housing_Project/LoginRegister.cs
+++ Housing_Project/Housing_Project/LoginRegister.cs
@@ -5,6 +5,114 @@ namespace Housing_Project
         public LoginRegister()
         {
             InitializeComponent();
+            ShowMessages(false);
+            ShowRegisterField(false);
+        }
+
+        private void ShowRegisterField(bool trueorfalse)
+        { 
+            fullnamelbl.Visible = trueorfalse;
+            fullnametxt.Visible = trueorfalse;
+            phonenumberlbl.Visible = trueorfalse;
+            phonenumbertxt.Visible = trueorfalse;
+            emailadresslbl.Visible = trueorfalse;
+            emailadresstxt.Visible = trueorfalse;
+            passwordtxt.Visible = trueorfalse;
+            passwordlbl.Visible = trueorfalse;
+            registerbtn.Visible = trueorfalse; // actual register btn
+            registerbtnshow.Visible = !trueorfalse; // show the register form btn
+
+            loginbtn.Visible = !trueorfalse; // actual log in btn
+            loginshowbtn.Visible = trueorfalse; // show the log in form
+
+        }
+        private void ShowLoginField(bool trueorfalse)
+        {
+            loginemaillbl.Visible = trueorfalse;
+            loginemailtxt.Visible = trueorfalse;
+            loginpasswordlbl.Visible = trueorfalse;
+            loginpasswordtxt.Visible = trueorfalse;
+
+            loginbtn.Visible = trueorfalse;
+            loginshowbtn.Visible = !trueorfalse;
+        }
+        private void ClearFields()
+        {
+            //Login field
+            loginpasswordtxt.Clear();
+            loginemailtxt.Clear();
+            //Register fields
+            fullnametxt.Clear();
+            emailadresstxt.Clear();
+            passwordtxt.Clear();
+            phonenumbertxt.Clear();
+        }
+        private void OpenStudents()
+        {
+            this.Hide();
+            FormStudent student = new FormStudent();
+            student.ShowDialog();
+            this.Close();
+        }
+        private void ShowMessages(bool trueorfalse) 
+        {
+            wrongcredentialslbl.Visible = trueorfalse;
+        }
+
+        private void loginshowbtn_Click(object sender, EventArgs e) // show log in form
+        {
+            ShowMessages(false);
+            ShowRegisterField(false);
+            ShowLoginField(true);
+
+        }
+
+        private void loginbtn_Click(object sender, EventArgs e) // actual log in button
+        {        
+            if (loginemailtxt.Text == "admin" && loginpasswordtxt.Text == "123")
+            {
+                OpenStudents();
+            }
+            else 
+            {
+                if (loginemailtxt.Text!="" || loginpasswordtxt.Text != "")
+                {
+                    ShowMessages(true);
+                    ClearFields();
+                }
+                else
+                {
+                    ClearFields();
+                }
+            }
+        }
+
+        private void registerbtn_Click_1(object sender, EventArgs e) // show register form
+        {
+            ShowMessages(false);
+            ShowRegisterField(true);
+            ShowLoginField(false);
+        }
+
+        private void registerbtn_Click(object sender, EventArgs e) // actial register
+        {      
+            //this.Hide()
+            //Student student = new Student();
+            //student.ShowDialog();
+            //this.Close();
+
+            if (fullnametxt.Text == "" 
+                || phonenumbertxt.Text == "" 
+                || emailadresstxt.Text == "" 
+                || passwordtxt.Text == "")
+            {
+                ShowMessages(true);
+            }
+            else
+            {
+                ClearFields();
+                OpenStudents();
+            }
         }
     }
 }

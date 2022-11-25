138006f2a6a8a96c3e8a2dc1ca200f368da15c31
Diana Ciuperca <dianav.ciuperca@gmail.com>
2022-11-25 10:47:24 +0100

    calendar

--- Housing_Project/Housing_Project/Supervisor.Designer.cs
+++ Housing_Project/Housing_Project/Supervisor.Designer.cs
@@ -32,6 +32,7 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
@@ -43,7 +44,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.tabControl1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage6.SuspendLayout();
@@ -55,9 +56,9 @@
             this.lblStudentHouseBV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lblStudentHouseBV.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblStudentHouseBV.Image = global::Housing_Project.Properties.Resources.house;
-            this.lblStudentHouseBV.Location = new System.Drawing.Point(12, 21);
+            this.lblStudentHouseBV.Location = new System.Drawing.Point(14, 28);
             this.lblStudentHouseBV.Name = "lblStudentHouseBV";
-            this.lblStudentHouseBV.Size = new System.Drawing.Size(907, 46);
+            this.lblStudentHouseBV.Size = new System.Drawing.Size(1036, 61);
             this.lblStudentHouseBV.TabIndex = 2;
             this.lblStudentHouseBV.Text = "Student House BV";
             // 
@@ -69,58 +70,76 @@
             this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Controls.Add(this.tabPage5);
             this.tabControl1.Controls.Add(this.tabPage6);
-            this.tabControl1.Location = new System.Drawing.Point(12, 88);
+            this.tabControl1.Location = new System.Drawing.Point(14, 117);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(907, 364);
+            this.tabControl1.Size = new System.Drawing.Size(1037, 485);
             this.tabControl1.TabIndex = 3;
             // 
             // tabPage1
             // 
             this.tabPage1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(899, 336);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage1.Size = new System.Drawing.Size(1029, 452);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Rules";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.monthCalendar1);
             this.tabPage2.Controls.Add(this.checkBox1);
             this.tabPage2.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(899, 336);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tabPage2.Size = new System.Drawing.Size(1029, 452);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Cleaning Schedule";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(543, 143);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(187, 51);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // tabPage3
             // 
-            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(899, 336);
+            this.tabPage3.Size = new System.Drawing.Size(1029, 452);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Agreements";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // tabPage4
             // 
-            this.tabPage4.Location = new System.Drawing.Point(4, 24);
+            this.tabPage4.Location = new System.Drawing.Point(4, 29);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(899, 336);
+            this.tabPage4.Size = new System.Drawing.Size(1029, 452);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Contact Info";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // tabPage5
             // 
-            this.tabPage5.Location = new System.Drawing.Point(4, 24);
+            this.tabPage5.Location = new System.Drawing.Point(4, 29);
+            this.tabPage5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Size = new System.Drawing.Size(899, 336);
+            this.tabPage5.Size = new System.Drawing.Size(1029, 452);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "Supplies";
             this.tabPage5.UseVisualStyleBackColor = true;
@@ -134,9 +153,10 @@
             this.tabPage6.Controls.Add(this.label3);
             this.tabPage6.Controls.Add(this.label2);
             this.tabPage6.Controls.Add(this.label1);
-            this.tabPage6.Location = new System.Drawing.Point(4, 24);
+            this.tabPage6.Location = new System.Drawing.Point(4, 29);
+            this.tabPage6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.tabPage6.Name = "tabPage6";
-            this.tabPage6.Size = new System.Drawing.Size(899, 336);
+            this.tabPage6.Size = new System.Drawing.Size(1029, 452);
             this.tabPage6.TabIndex = 5;
             this.tabPage6.Text = "Report a problem";
             this.tabPage6.UseVisualStyleBackColor = true;
@@ -144,9 +164,10 @@
             // report_problem
             // 
             this.report_problem.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.report_problem.Location = new System.Drawing.Point(287, 237);
+            this.report_problem.Location = new System.Drawing.Point(328, 316);
+            this.report_problem.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.report_problem.Name = "report_problem";
-            this.report_problem.Size = new System.Drawing.Size(323, 68);
+            this.report_problem.Size = new System.Drawing.Size(369, 91);
             this.report_problem.TabIndex = 6;
             this.report_problem.Text = "Report a problem";
             this.report_problem.UseVisualStyleBackColor = true;
@@ -154,35 +175,38 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(3, 112);
+            this.richTextBox1.Location = new System.Drawing.Point(3, 149);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(881, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(1006, 127);
             this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox2.Location = new System.Drawing.Point(538, 26);
+            this.textBox2.Location = new System.Drawing.Point(615, 35);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(145, 35);
+            this.textBox2.Size = new System.Drawing.Size(165, 42);
             this.textBox2.TabIndex = 4;
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox1.Location = new System.Drawing.Point(196, 23);
+            this.textBox1.Location = new System.Drawing.Point(224, 31);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(129, 35);
+            this.textBox1.Size = new System.Drawing.Size(147, 42);
             this.textBox1.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(364, 26);
+            this.label3.Location = new System.Drawing.Point(416, 35);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(168, 30);
+            this.label3.Size = new System.Drawing.Size(213, 37);
             this.label3.TabIndex = 2;
             this.label3.Text = "Nr of apartment:";
             // 
@@ -190,9 +214,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(127, 23);
+            this.label2.Location = new System.Drawing.Point(145, 31);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 30);
+            this.label2.Size = new System.Drawing.Size(94, 37);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name:";
             // 
@@ -200,29 +224,26 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(3, 79);
+            this.label1.Location = new System.Drawing.Point(3, 105);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(267, 30);
+            this.label1.Size = new System.Drawing.Size(342, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "Description of the problem:";
             // 
-            // checkBox1
+            // monthCalendar1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(475, 107);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(145, 40);
-            this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.monthCalendar1.Location = new System.Drawing.Point(32, 35);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
             // 
             // Supervisor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(932, 486);
+            this.ClientSize = new System.Drawing.Size(1065, 648);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.lblStudentHouseBV);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Supervisor";
             this.Text = "Supervisor";
             this.Load += new System.EventHandler(this.Supervisor_Load);
@@ -253,5 +274,6 @@
         private Label label2;
         private Label label1;
         private CheckBox checkBox1;
+        private MonthCalendar monthCalendar1;
     }
 }

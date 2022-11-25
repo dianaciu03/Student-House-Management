135152261e06d71057a9014356344066d3021d5a
Draganova,Denitsa D.M <d.draganova@student.fontys.nl>
2022-11-25 11:31:58 +0200

    Supervisor interface

--- Housing_Project/Housing_Project/Supervisor.Designer.cs
+++ Housing_Project/Housing_Project/Supervisor.Designer.cs
@@ -28,12 +28,214 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblStudentHouseBV = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.report_problem = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblStudentHouseBV
+            // 
+            this.lblStudentHouseBV.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.lblStudentHouseBV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblStudentHouseBV.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblStudentHouseBV.Image = global::Housing_Project.Properties.Resources.house;
+            this.lblStudentHouseBV.Location = new System.Drawing.Point(12, 21);
+            this.lblStudentHouseBV.Name = "lblStudentHouseBV";
+            this.lblStudentHouseBV.Size = new System.Drawing.Size(907, 46);
+            this.lblStudentHouseBV.TabIndex = 2;
+            this.lblStudentHouseBV.Text = "Student House BV";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage5);
+            this.tabControl1.Controls.Add(this.tabPage6);
+            this.tabControl1.Location = new System.Drawing.Point(12, 88);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(907, 364);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(899, 336);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Rules";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(899, 336);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Cleaning Schedule";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 24);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(899, 336);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Agreements";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 24);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(899, 336);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Contact Info";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Location = new System.Drawing.Point(4, 24);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(899, 336);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Supplies";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.report_problem);
+            this.tabPage6.Controls.Add(this.richTextBox1);
+            this.tabPage6.Controls.Add(this.textBox2);
+            this.tabPage6.Controls.Add(this.textBox1);
+            this.tabPage6.Controls.Add(this.label3);
+            this.tabPage6.Controls.Add(this.label2);
+            this.tabPage6.Controls.Add(this.label1);
+            this.tabPage6.Location = new System.Drawing.Point(4, 24);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(899, 336);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Report a problem";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // report_problem
+            // 
+            this.report_problem.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.report_problem.Location = new System.Drawing.Point(287, 237);
+            this.report_problem.Name = "report_problem";
+            this.report_problem.Size = new System.Drawing.Size(323, 68);
+            this.report_problem.TabIndex = 6;
+            this.report_problem.Text = "Report a problem";
+            this.report_problem.UseVisualStyleBackColor = true;
+            this.report_problem.Click += new System.EventHandler(this.report_problem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 112);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(881, 96);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.Location = new System.Drawing.Point(538, 26);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 35);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(196, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 35);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(364, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(168, 30);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nr of apartment:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(127, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(3, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(267, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Description of the problem:";
+            // 
+            // Supervisor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(932, 486);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.lblStudentHouseBV);
+            this.Name = "Supervisor";
             this.Text = "Supervisor";
+            this.Load += new System.EventHandler(this.Supervisor_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage6.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Label lblStudentHouseBV;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private TabPage tabPage3;
+        private TabPage tabPage4;
+        private TabPage tabPage5;
+        private TabPage tabPage6;
+        private Button report_problem;
+        private RichTextBox richTextBox1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Label label1;
     }
 }
--- Housing_Project/Housing_Project/Supervisor.cs
+++ Housing_Project/Housing_Project/Supervisor.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace Housing_Project
 {
@@ -15,6 +17,85 @@ namespace Housing_Project
         public Supervisor()
         {
             InitializeComponent();
+        }
+
+        private void Supervisor_Load(object sender, EventArgs e)
+        {
+            //DataTable dt = new DataTable();
+            //dt.Columns.AddRange(new DataColumn[3] { new DataColumn("Id", typeof(int)),
+            //            new DataColumn("Name", typeof(string)),
+            //            new DataColumn("Country",typeof(string)) });
+            //dt.Rows.Add(1, "John Hammond", "United States");
+            //dt.Rows.Add(2, "Mudassar Khan", "India");
+            //dt.Rows.Add(3, "Suzanne Mathews", "France");
+            //dt.Rows.Add(4, "Robert Schidner", "Russia");
+            //this.dataGridView1.DataSource = dt;
+            //this.dataGridView1.AllowUserToAddRows = false;
+        }
+
+        private void report_problem_Click(object sender, EventArgs e)
+        {
+            //try
+
+            //{
+
+            //    String str = "";
+
+            //    String query = "select * from data";
+
+            //    SqlConnection con = new SqlConnection(str);
+
+            //    SqlCommand cmd = new SqlCommand(query, con);
+
+            //    con.Open();
+
+            //    DataSet ds = new DataSet();
+
+            //    MessageBox.Show("connect with sql server");
+
+            //    con.Close();
+
+            //}
+
+            //catch (Exception es)
+
+            //{
+
+            //    MessageBox.Show(es.Message);
+
+
+
+            //}
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        //    //Table start.
+        //    string html = "<table cellpadding='5' cellspacing='0' style='border: 1px solid #ccc;font-size: 9pt;font-family:arial'>";
+
+        //    //Adding HeaderRow.
+        //    html += "<tr>";
+        //    foreach (DataGridViewColumn column in dataGridView1.Columns)
+        //    {
+        //        html += "<th style='background-color: #B8DBFD;border: 1px solid #ccc'>" + column.HeaderText + "</th>";
+        //    }
+        //    html += "</tr>";
+
+        //    //Adding DataRow.
+        //    foreach (DataGridViewRow row in dataGridView1.Rows)
+        //    {
+        //        html += "<tr>";
+        //        foreach (DataGridViewCell cell in row.Cells)
+        //        {
+        //            html += "<td style='width:120px;border: 1px solid #ccc'>" + cell.Value.ToString() + "</td>";
+        //        }
+        //        html += "</tr>";
+        //    }
+
+        //    //Table end.
+        //    html += "</table>";
+
+        //    File.WriteAllText(@"E:\Files\DataGridView.htm", html);
         }
     }
 }
